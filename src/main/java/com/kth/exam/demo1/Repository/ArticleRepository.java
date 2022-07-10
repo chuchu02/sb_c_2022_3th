fd19a176213d67c689e779cafba4cb2df3fb50c5
chuchu02 <rlaxogns73@gmail.com>
2022-07-11 02:29:45 +0900

    SQL xml파일로 옮김

--- src/main/java/com/kth/exam/demo1/Repository/ArticleRepository.java
+++ src/main/java/com/kth/exam/demo1/Repository/ArticleRepository.java
@@ -13,25 +13,15 @@ import com.kth.exam.demo1.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	// INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, `body` = ?;
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
 	public void writeArticle(@Param("title") String title, @Param("body") String body);
 
-	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
-	// DELETE FROM article WHERE id = ?
-	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(@Param("id") int id);
 
-	// UPDATE article SET title = ?, `body` = ?, updateDate = NOW() WHERE id = ?;
-	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
 	public void modify(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 	
-	// SELECT * FROM article ORDER BY id DESC;
-	@Select("SELECT * FROM article ORDER BY id DESC;")
 	public List<Article> getArticles();
 
-	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();
 }
