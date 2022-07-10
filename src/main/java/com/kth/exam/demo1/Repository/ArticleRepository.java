1296a001e06ced2fd597d7300b9181b8442ccfd9
chuchu02 <rlaxogns73@gmail.com>
2022-07-10 22:55:55 +0900

    getArticles 목록 10개 불러오기

---
+++ src/main/java/com/kth/exam/demo1/Repository/ArticleRepository.java
@@ -0,0 +1,33 @@
+package com.kth.exam.demo1.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.kth.exam.demo1.vo.Article;
+
+@Component
+public class ArticleRepository {
+
+	private List<Article> articles;
+	private int lastId;
+	
+	public ArticleRepository() {
+		articles = new ArrayList<>();
+		lastId = 0;
+	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public void writeArticle(String title, String body) {
+		int id = lastId + 1;
+		Article article = new Article(id, title, body);
+		articles.add(article);
+		
+		lastId = id;
+	}
+
+}
--- src/main/java/com/kth/exam/demo1/service/ArticleService.java
+++ src/main/java/com/kth/exam/demo1/service/ArticleService.java
@@ -5,12 +5,31 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.kth.exam.demo1.vo.Article;
+import com.kth.exam.demo1.Repository.ArticleRepository;
 
 @Service
 public class ArticleService {
+	private ArticleRepository articleRepository;
+	
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for(int i = 1; i <= 10; i++) {
+			String title = "제목 " + i;
+			String body = " 내용 " + i;
+			writeArticle(title, body);
+		}
+	}
+	
+	public void writeArticle(String title, String body) {
+		articleRepository.writeArticle(title, body);
+	}
 
 	public List<Article> getArticles() {
-		return null;
+		return articleRepository.getArticles();
 	}
 
 }
