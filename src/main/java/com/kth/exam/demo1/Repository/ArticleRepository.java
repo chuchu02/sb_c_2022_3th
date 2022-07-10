47cd9a8d09224dc4aaa3f7ff36ef3c58d8af82b1
chuchu02 <rlaxogns73@gmail.com>
2022-07-11 02:02:41 +0900

    삭제, 수정, 조회 메서드 연결

--- src/main/java/com/kth/exam/demo1/Repository/ArticleRepository.java
+++ src/main/java/com/kth/exam/demo1/Repository/ArticleRepository.java
@@ -1,55 +1,32 @@
 package com.kth.exam.demo1.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.kth.exam.demo1.vo.Article;
 
-@Component
-public class ArticleRepository {
+@Mapper
+public interface ArticleRepository {
+	// INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, `body` = ?;
+	public Article writeArticle(String title, String body);
 
-	private List<Article> articles;
-	private int lastId;
+	@Select("SELECT * FROM article WHERE id = #{id}")
+	public Article getArticle(@Param("id") int id);
 
-	public ArticleRepository() {
-		articles = new ArrayList<>();
-		lastId = 0;
-	}
+	// DELETE FROM article WHERE id = ?
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(@Param("id") int id);
 
-	public List<Article> getArticles() {
-		return articles;
-	}
-
-	public void writeArticle(String title, String body) {
-		int id = lastId + 1;
-		Article article = new Article(id, title, body);
-		articles.add(article);
-
-		lastId = id;
-	}
-
-	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-
-	public void delete(int id) {
-		Article article = getArticle(id);
-
-		articles.remove(article);
-	}
-
-	public void modify(int id, String title, String body) {
-		Article article = getArticle(id);
-		
-		article.setTitle(title);
-		article.setBody(body);
-	}
-
+	// UPDATE article SET title = ?, `body` = ?, updateDate = NOW() WHERE id = ?;
+	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
+	public void modify(@Param("id") int id, @Param("title") String title, @Param("body") String body);
+	
+	// SELECT * FROM article ORDER BY id DESC;
+	@Select("SELECT * FROM article ORDER BY id DESC;")
+	public List<Article> getArticles();
 }
--- src/main/java/com/kth/exam/demo1/service/ArticleService.java
+++ src/main/java/com/kth/exam/demo1/service/ArticleService.java
@@ -13,15 +13,6 @@ public class ArticleService {
 
 	public ArticleService(ArticleRepository articleRepository) {
 		this.articleRepository = articleRepository;
-		makeTestData();
-	}
-
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목 " + i;
-			String body = " 내용 " + i;
-			writeArticle(title, body);
-		}
 	}
 
 	public void writeArticle(String title, String body) {
@@ -37,7 +28,7 @@ public class ArticleService {
 	}
 
 	public void delete(int id) {
-		articleRepository.delete(id);
+		articleRepository.deleteArticle(id);
 	}
 
 	public void modify(int id, String title, String body) {
